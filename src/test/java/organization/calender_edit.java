9703edc9a6e1ab254bea3e2d036e0ac88750a9a8
User <User@DESKTOP-TG69BTG>
2025-12-27 10:41:56 +0530

    start project

---
+++ src/test/java/organization/calender_edit.java
@@ -0,0 +1,11 @@
+package organization;
+
+public class calender_edit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-----bye bye 2025-------");
+
+	}
+
+}
